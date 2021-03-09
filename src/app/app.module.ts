424a2fef5bc1ac4855523283240b4fcb19eeaa58
Gabriel Silveira <dsilvermax@gmail.com>
2021-03-09 01:55:01 -0300

    push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TreeComponent } from './tree/tree.component';
+import { CdkTreeModule } from '@angular/cdk/tree'; 
+import { MatTreeModule } from '@angular/material/tree';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TreeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CdkTreeModule,
+    MatTreeModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatIconModule,
+    MatCheckboxModule
+  ],
+  exports: [
   ],
   providers: [],
   bootstrap: [AppComponent]
